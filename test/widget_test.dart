3b432f6f53d2a2f332cdfbed0ee2ec19d8c9b4bc
Farrukh Faraz <devfarrukh2@gmail.com>
2025-03-22 17:28:33 +0500

    updated

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:sms_owner/main.dart';
+import 'package:sms_owner/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
